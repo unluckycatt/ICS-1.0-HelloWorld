e818614efbfde6a7e5a252200a9ad03b5e871bce
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-05 16:11:49 +0000

    Made Some Updates

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,3 @@
 // Log "Hello World" to the console.
-
 // Log four additional messages of your choosing. 
 // Comment each message appropriately. 
-// Run the program by pressing the play button at the top right of this window. Look at the 'Output' below to see the output.
