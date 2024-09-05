898595fe308c119a18aeca9c2442088c0fcc7983
unluckycatt <1mannallaid@hdsb.ca>
2024-09-05 19:04:37 +0000

    more and more words

--- helloworld.js
+++ helloworld.js
@@ -3,3 +3,17 @@
 // Comment each message appropriately. 
 console.log("Testing!");
 console.log("This is a change!");
+console.log("");
+console.log("this is not a change!");
+console.log("");
+console.log("help me pls i need help");
+console.log("i need help");
+console.log("");
+console.log("plssssssssss");
+console.log("");
+console.log("i need sleep ngl");
+console.log("");
+console.log("ooooooooooooooooooooooooo");
+console.log("cant bleve it i win");
+console.log("");
+console.log("nah id wid");
